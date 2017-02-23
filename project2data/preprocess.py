ed51f59890558297367788fbb3f46bae1666f586
someone250 <ktat@caltech.edu>
2017-02-22 16:27:21 -0800

    Fixed syllable counter to work with made-up words. Return list of syllables

--- project2data/preprocess.py
+++ project2data/preprocess.py
@@ -1,12 +1,27 @@
 import numpy as np
+import string
 from hmmlearn import hmm
 from nltk.corpus import cmudict
 # nltk.download('book')
 
-#finds the number of syllables in a word. 
+# finds the number of syllables in a word.
 d = cmudict.dict()
+
+'''
+Works for nonwords now by checking if KeyError
+If word not real, remove last character and try again
+'''
 def nsyl(word):
-  return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]] 
+    try:
+        s = [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
+    except KeyError:
+        new_word = word[:-1]
+        s = nsyl(new_word)
+    return s
+
+# s is a string of words
+def strip_punc(s):
+    return s.translate(string.maketrans("",""), string.punctuation)
 
 
 def separate_sonnets(filename):
@@ -16,7 +31,6 @@ def separate_sonnets(filename):
 
     bad_list = [str(i) for i in xrange(155)]
     good_lines = []
-    corpus_lines = []
 
     # collect lines in sonnets 99 and 126
     for i in xrange(len(lines)):
@@ -30,7 +44,17 @@ def separate_sonnets(filename):
     # put non-number lines and lines not in sonn. 99 and 126 in good_lines
     for i in xrange(len(lines)):
         if lines[i] not in bad_list:
-            good_lines.append(lines[i])
+            good_lines.append(strip_punc(lines[i]).split())
+
+    syllables = []
+    for i in xrange(len(good_lines)):
+        syls = []
+        for j in xrange(len(good_lines[i])):
+            num_syls = nsyl(good_lines[i][j])
+            syls.append(num_syls)
+        syllables.append(syls)
+
+    return good_lines
 
 
 def main():
---
+++ project2data/syl.py
@@ -0,0 +1,24 @@
+# file for testing syllable issue on fake words
+
+from nltk.corpus import cmudict
+
+# finds the number of syllables in a word.
+d = cmudict.dict()
+
+# does not work if word is not recognized in NLTK
+def nsyl(word):
+    try:
+        s = [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
+    except KeyError:
+        new_word = word[:-1]
+        s = nsyl(new_word)
+    return s
+
+
+def main():
+    strings = ['dogs', 'beauty', 'tomato', 'showst']
+
+    for string in strings:
+        print nsyl(string)
+
+main()
